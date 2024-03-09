3750811568a19201af85c44cb1ff06d84edc24cd
cmyk52 <j.lmarambio@hotmail.com>
2024-03-09 14:38:17 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {
+        colors:{
+          gafp: '#81bd00',
+          oafp: '#ff9100',
+        }
+      },
+  },
+  plugins: [],
+}
